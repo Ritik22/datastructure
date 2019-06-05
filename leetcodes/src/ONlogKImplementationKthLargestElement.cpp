7eed0a94de0f4a578565d47d2fe4fe5098f91a90
Kavikant Chavan <Kavikant.Chavan@telusinternational.com>
2019-06-05 16:54:08 +0530

    Actual implementation of o(nlogk)

--- leetcodes/src/ONlogKImplementationKthLargestElement.cpp
+++ leetcodes/src/ONlogKImplementationKthLargestElement.cpp
@@ -1,10 +1,63 @@
+//Leet code problem number 215 implementation
+//Using min heap priority queue
+//O ( nlogk) - complexity
+
 #include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
 class Solution
 {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int> ,greater<int> > pq;
+        int len = nums.size();
+        for(int i = 0; i < len ; i++)
+        {
+            if(pq.size() < k)
+            {
+                pq.push(nums[i]);
+            }
+            else
+            {
+                int elem = pq.top();
+                if(elem < nums[i])
+                {
+                    pq.pop();
+                    pq.push(nums[i]);
+                }
+            }
+        }
+        return pq.top();
+    }
 };
 
 int main()
 {
+    int k,elem;
+    char choice;
+    vector <int> vec;
+    Solution s;
+
+
+    while(choice != 'n')
+    {
+        cout<<"\nProvide vector element "<<endl;
+        cin>>elem;
+        vec.push_back(elem);
+        cout<<"\nDo you want to continute (y/n) ";
+        cin>>choice;
+    }
+    cout<<"\n Vector elements are  " ;
+    for(auto val:vec)
+    {
+        cout<<val<<"  ";
+    }
+    cout<<"\nPlease provide Kth element value ";
+    cin>>k;
+
+    cout<<"\nValue of Kth Largest element is "<<s.findKthLargest(vec,k);
+    cout<<endl;
 }
